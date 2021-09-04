2af9abcf3ce6589e30056cb879b74373825e13d0
Pablo Cuevas <cuevasgpablo@gmail.com>
2021-09-04 17:20:43 -0400

    Agregado Barra de navegación, footer, y formulario de inicio de sesión

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,131 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <!-- BARRA DE NAVEGACION -->
+    <div id="nav">
+        <nav
+            class="navbar navbar-expand-lg navbar-dark sticky-top"
+            style="background-color: slateblue"
+        >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img
+                        src="@/assets/img/astraLogoSlate.png"
+                        alt=""
+                        width="150"
+                    />
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <!-- <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="inicio.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="miCuenta.html">Mi Cuenta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="grupos.html">Grupos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="amigos.html">Amigos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="novedades.html">Novedades</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contacto.html">Contacto</a>
+                </li> -->
+                    </ul>
+                    <div class="d-flex">
+                        <!-- <input class="form-control me-2" type="search" placeholder="" aria-label="Search" id="texto" name="texto">
+                    <button class="btn btn-outline-light text-nowrap" type="submit"><i class="fas fa-search"></i> Buscar</button> |
+                <a class="btn btn-outline-light text-light" type="submit"><i class="fas fa-power-off"></i></a> -->
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!-- CONTENIDO DE LA PAGINA -->
+    <div :style="imagenFondo">
+        <router-view class="container" />
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="footer mt-auto py-3 text-light" style="background-color: rgb(53, 45, 102);">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h5>AstraVita</h5>
+                    <span class="text-light">
+                        <ul class="list-group list-group-flush">
+                            <li>
+                                <i class="fas fa-map-marked-alt"></i> <a class="text-light" href="https://goo.gl/maps/1exkBhNeogh4EjnW7">Calle Falsa #123</a>
+                            </li>
+                            <li>
+                                <i class="fas fa-phone"></i> <a class="text-light" href="tel:+56978940490">+569 7894 0490</a>
+                            </li>
+                            <li>
+                                <i class="fas fa-at"></i> <a href="mailto:soporte@astravita.cl"
+                                    class="text-light">soporte@astravita.cl</a>
+                            </li>
+                            <li>
+                                <i class="fas fa-heart"></i> <a class="text-light"
+                                    href="suscripcion.html">Suscríbete</a>
+                            </li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="col">
+                    <h5>Nosotros</h5>
+                    <span class="text-light">
+                        <ul class="list-group list-group-flush">
+                            <li>
+                                <i class="fab fa-instagram"></i> <a class="text-light"
+                                    href="https://www.instagram.com/sebastian.vergara.nunez/?hl=es-la">Sebastián
+                                    Vergara</a>
+                            </li>
+                            <li>
+                                <i class="fab fa-instagram"></i> <a class="text-light"
+                                    href="https://www.instagram.com/rusioflores/?hl=es-la">Matías Flores</a>
+
+                            </li>
+                            <li>
+                                <i class="fab fa-instagram"></i> <a class="text-light"
+                                    href="https://www.instagram.com/pablocg.uwu/?hl=es-la">Pablo Cuevas</a>
+                            </li>
+                        </ul>
+                    </span>
+                </div>
+                <div class="col text-center">
+                    <span class="text-muted text-center">Proyecto AstraVita&copy;</span>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style>
+<script>
+import { computed } from "vue";
 
-</style>
+export default {
+    setup() {
+        const imagenFondo = computed(() => {
+            return {
+                backgroundImage: `url(${require("@/assets/img/universo.jpg")})`,
+            };
+        });
+
+        return { imagenFondo };
+    },
+};
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+     <div class="d-flex align-items-center justify-content-center container" style="height: 800px">
+        <div class="p-5 mb-4 rounded-3" style="background-color: rgba(7, 0, 44, 0.5);">
+            <div class="container-fluid row">
+                <div class="col-md-6">
+                    <img src="@/assets/img/zodiac.jpg" th:src="@{/img/profelogo.png}" class="img-fluid">
+                </div>
+                <div class="col-md-6 text-light">
+                    <h1 class="display-5 fw-bold text-light text-center">¡Bienvenido!</h1>
+                    <form class="mt-lg-5">
+                        <div class="row mb-3">
+                            <label for="inputEmail3" class="col-sm-2 col-form-label">Usuario</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control rounded-pill" id="inputEmail3">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">Clave</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control rounded-pill" id="inputPassword3">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <button type="submit" class="btn d-block text-light rounded-pill"
+                                style="background-color: slateblue;">Iniciar Sesión</button>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Recuerdame
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-6 text-md-end">
+                                <a href="http://" class="text-light">¿Olvidaste tu clave?</a>
+                            </div>
+                        </div>
+                        <div class="row mb-3 text-light">
+                            <div class="col">
+                                ¿No posees una cuenta?, <a href="registro.html" class="text-light">Registrate</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
