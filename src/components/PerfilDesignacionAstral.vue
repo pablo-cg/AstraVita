91b7ea997889527c501f3b9e1cee7cc9f365da1a
Pablo Cuevas <cuevasgpablo@gmail.com>
2021-09-20 19:46:26 -0300

    Cambios: - Eliminados del metodto test codigo que no funciona, solo esta comentado codigo para subir la imagen al bucket de storage (PerfilDesignacionAstral)

--- src/components/PerfilDesignacionAstral.vue
+++ src/components/PerfilDesignacionAstral.vue
@@ -80,7 +80,6 @@
 </template>
 
 <script>
-import { supabase } from "../includes/supabase";
 
 export default {
     props: ["planetas", "casas", "imagenRuedaCarta"],
@@ -93,30 +92,19 @@ export default {
             return normal.replace(" ", "");
         },
         async test() {
-            const opcionesRequest = {
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                },
-            };
-            
-            const img = await fetch("https://v70ppwoy52.execute-api.eu-west-1.amazonaws.com/stage/chart?payload=NYA4Jx8Cpeqom%2FDtMO0EKfEkAtojPVO3C3hsWZc%2FbWi3erm0uQav9o0kM8eGqpbVQ1MxzfpwNq%2F6XlcECKz9bPz9CCqKymnWSM9%2FKMJzD2josehq7kg4Vpc7Er%2FyhRoeEdLm2zEAgsdMIuC03L0C91WFksp6R2L8oLnTu1Z8c8XJqWbsX2%2FnGeVovsEccBlMrwGVn1C2SETwJ4%2BBX9pjrFMPTAw8m5SfqM2yA71hv0o%2FE7TAjVjjnK3tCxpGZwCsiwJwI5aPnmcMVsLrGAUyTqOP6gKjxCYU3ZnYEgIYO2LGDKgFrBfF3ljRvrmhEEcHawVqspg8QoL2Dm5S0MX9Yi637S12RFyfNsHz%2B7fepsXe8ETtjj4CYQC8jV9taEKqN9ygn2a1YTWCD%2BgcLkTTP3knqXSrq3l2zFxKM2B0nF1zV5vFg3tEg9bdxplxeoZMMK2Y53MzrdWfHqgLH%2FCOQN7WY0O5fit33tM2po%2F1bivaPFzNgFGxgVMeegEB79%2B1%2FpDlc1SnTnS1nzsirYWI4LIQ61eWvxfLhi6IA1SAoqWGCTpHeNGeUa3hHspaDat9DtZ4xuPqajElkVA3ZPzlRp4pslXdq%2B7sL2T0wOd1vDUctbFJJ89lt4G4D1RPYiuhLidAtlW1V4tCayT61EWGQl0xbdNQdBDE7VTG5b%2BuS%2FJcWo9I9gnrsEPyKrzdGmoeUya%2FdRTBgg8POJqMmEOJjeZNiGX5XLL%2FjY18bQTLhzCihxaBravLQkIl%2FnIqbaV7Zs3qSa1iWspAdmwCLzHmzSF1jc5P8XEXslLsnzP8RPApfGyjV3Eu3jp9oGEL70mq2sW09aDSR5NVvkFWc0Osc2QogDzfyRYh94eVq47KWF3lAGm9tVh3%2Be%2FkkpXaZs12Leg69Tc3TxYq%2BbB1oa%2FDvQrXmqENUdK9J%2BVKhpRRK0FYRirtU45GBuRBqAScEZ%2B0gzZFpXZUPg106I00Tg4KLlStJ9X6xpkBToDuVJusFbp0TwZjmh7rZ%2B5IXBo1YmrJ2V%2BxDUE4Qp%2BcEPPqWfeDu8vvM1ZDYz2HZY4QTqUL33KSJ5EIkRHtw%2F5PFFcwl0XKFAPeITTxoblAoVHmvIZMJPeSW12n8vFpjpTdobnqhAclniMkMPQXWFVKZaQ%2Fu67UER%2FaHEW1FQl2qpAEzruJ369TeYRCntRLN3FF9w2eqkIJKb4rIMMgEImdpaN885l7adDNHDRYDph4KsEdPSvXegQaJLDUBCQC0IGcvYNVR3usqhtPs4iAtBZMSKxNhrnOAGEk22OjL06%2FEOqOgSgAhWr2CD3E1l760Ib%2FkdZEE0Kvl8c%2FQclCYHmJq4Bc%2BLfDP68prMwODlXVLamvOuIxDb%2F6%2B%2Bpi4uZ0XvT4Oqk388fur%2FA3y7F0WKqyzF9PNx5pohtDwI1MjpxPveG0z64PM%2BHFNKTJesSbidRg4y80ii8VArDnW5MKLRhkLHcBhWyo07uF1RzTU1e%2B7LG1F7CrTPJFuoC7nHb5OeS8IeyQKYmX1p1VTaQRQUzHT9%2BWLmxUMBIBjKnm%2Ff5buHfvdxfkWCM6Pieb6iKoy%2BOF5%2BRzeD07OACH6se48v%2BhZcHv&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhc3Ryb2FwaSIsImlhdCI6MTYzMjAyMjQyMiwiZXhwIjoxNjMyMDIyNzIyfQ.FtOnzrYAhKz68kuUjBOYZd08hCoi3oMn-v445yoByds", opcionesRequest);
             // const imgblob = await img.blob();
             // console.log(imgblob);
-
             // const { data, error } = await supabase.storage
             //     .from("astra_bucket")
             //     .upload("wheels/avatar2.svg", imgblob);
-
             // if(error){
             //     console.log(error);
             // }else{
             //     console.log(data);
             // }
-
             // const { data, error } = supabase.storage
             //     .from("public_bucket")
             //     .download("wheels/avatar1.jpeg");
-
             // if (error) {
             //     console.log(error);
             // } else {
