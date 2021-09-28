0dd4e00300ba6d6e4dbe5239fb8d887c70bd2959
Pablo Cuevas <cuevasgpablo@gmail.com>
2021-09-28 20:49:33 -0300

    Cambios: - Agregada la dependencia vuex-persistedstate para la persistencia de la sesión - Modificado el index del store para agregar el persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+// import SecureLS from "secure-ls";
 import usuarioStore from './modules/usuarioStore';
 import cartaAstralStore from './modules/cartaAstralStore';
+
+// var ls = new SecureLS({ isCompression: false });
 
 export default createStore({
     modules: {
         usuarioStore,
         cartaAstralStore
     },
+    // plugins : [createPersistedState({
+    //     storage: {
+    //         getItem: (key) => ls.get(key),
+    //         setItem: (key, value) => ls.set(key, value),
+    //         removeItem: (key) => ls.remove(key),
+    //       },
+    // })],
+    plugins : [createPersistedState()],
 })
