b467abd00cb7382e988ab04c2dc9996a96b3ead6
Pablo Cuevas <cuevasgpablo@gmail.com>
2021-11-15 18:43:17 -0300

    Cambios: - Creado el formulario de contacto para el usuario

--- src/views/VistaContacto.vue
+++ src/views/VistaContacto.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="row row-cols-1 row-cols-md-1 g-2">
+    <div class="row row-cols-1 row-cols-md-1 g-2">
         <div
             class="mt-4 p-5 text-white rounded"
             style="background-color: #5548a4; border-color: slateblue"
@@ -14,12 +14,63 @@
                     </button>
                 </div> -->
             </div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente cupiditate nemo quae ipsa. Quaerat sapiente soluta
-                veritatis corporis labore obcaecati, itaque sunt quos earum
-                blanditiis eveniet dolorum cupiditate pariatur harum.
+            <p class="fs-5">
+                ¿Tienes algún inconveniente?, cuéntanos de qué se trata y te
+                ayudaremos
             </p>
         </div>
     </div>
+    <div class="row row-cols-1 row-cols-md-1 g-2">
+        <div class="card mt-3">
+            <div class="card-body">
+                <form>
+                    <div class="mb-4">
+                        <label for="asunto" class="form-label">Asunto:</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="asunto"
+                            aria-describedby="ayudaAsunto"
+                        />
+                        <div id="ayudaAsunto" class="form-text">
+                            Cuéntanos en pocas palabras de que se trata
+                        </div>
+                    </div>
+                    <div class="mb-3 form-floating">
+                        <textarea
+                            class="form-control"
+                            id="detalles"
+                            style="height: 100px"
+                        ></textarea>
+                        <label for="detalles">Pon aquí los detalles:</label>
+                    </div>
+                    <button type="submit" class="btn astra-btn-primario">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
+<style scoped>
+.astra-btn-primario {
+    background-color: slateblue;
+    border-color: slateblue;
+    color: whitesmoke;
+}
+
+.astra-btn-primario:hover {
+    background-color: whitesmoke;
+    border-color: slateblue;
+    color: black;
+}
+
+.astra-btn-secundario {
+    background-color: whitesmoke;
+    border-color: slateblue;
+    color: black;
+}
+.astra-btn-secundario:hover {
+    background-color: slateblue;
+    border-color: slateblue;
+    color: whitesmoke;
+}
+</style>
