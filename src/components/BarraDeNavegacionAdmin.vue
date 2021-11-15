142b3d3acdf1bfbf473cdeca89a6b250215e9d5b
Pablo Cuevas <cuevasgpablo@gmail.com>
2021-11-15 18:14:59 -0300

    Cambios: - Modificada la barra de navegación del admin - Agregado filtro y modal de eliminación en VistaListaUsuario

--- src/components/BarraDeNavegacionAdmin.vue
+++ src/components/BarraDeNavegacionAdmin.vue
@@ -22,17 +22,41 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <span class="nav-link">{{usuario.nombre}}</span>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{ name: 'ListaUsuarios' }"
-                            >Usuarios</router-link
-                        >
+                        <span class="nav-link">{{ usuario.nombre }}</span>
                     </li>
                     <li class="nav-item">
                         <router-link class="nav-link" :to="{ name: 'Soporte' }"
                             >Soporte</router-link
                         >
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a
+                            class="nav-link dropdown-toggle"
+                            href="#"
+                            id="navbarDropdown"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            Usuarios
+                        </a>
+                        <ul
+                            class="dropdown-menu"
+                            aria-labelledby="navbarDropdown"
+                        >
+                            <li>
+                                <router-link
+                            class="dropdown-item"
+                            :to="{ name: 'ListaUsuarios' }"
+                            ><i class="fas fa-list-ol"></i> Lista de usuarios</router-link
+                        >
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#"
+                                    ><i class="fas fa-user-plus"></i> Crear Nuevo usuario</a
+                                >
+                            </li>
+                        </ul>
                     </li>
                 </ul>
                 <button
--- src/views/VistaListaUsuario.vue
+++ src/views/VistaListaUsuario.vue
@@ -5,32 +5,42 @@
     >
         <div class="row">
             <div class="col">
-                <h1>Usuarios</h1>
+                <h1>Lista de usuarios</h1>
             </div>
-        </div>
-        <div class="row mt-4" v-if="usuarios">
-            <h5 class="col">
-                <span class="badge bg-light text-dark"
-                    >Usuarios Totales: {{ usuarios.length }}</span
-                >
+            <h5 class="col text-end">
+                <span class="badge bg-light text-dark" v-if="usuarios">
+                    Usuarios Totales: {{ usuarios.length }}
+                </span>
             </h5>
         </div>
     </div>
     <div class="card mt-3">
         <div class="card-body">
+            <div class="input-group my-2">
+                <span class="input-group-text">Filtrar</span>
+                <input
+                    type="text"
+                    class="form-control border-dark"
+                    placeholder="Ingresa un nombre"
+                    v-model="filtro"
+                />
+            </div>
             <div class="table-responsive">
                 <table class="table table-hover table-bordered">
                     <thead class="text-center">
                         <tr>
-                            <th scope="col">UUID</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Correo</th>
-                            <th scope="col">Estado Suscripción</th>
-                            <th scope="col" colspan="2">Acciones</th>
+                            <th>UUID</th>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                            <th>Estado Suscripción</th>
+                            <th colspan="2">Acciones</th>
                         </tr>
                     </thead>
                     <tbody v-if="usuarios" class="align-middle">
-                        <tr v-for="usuario in usuarios" :key="usuario.id">
+                        <tr
+                            v-for="usuario in usuariosFiltrados"
+                            :key="usuario.id"
+                        >
                             <td>{{ usuario.id }}</td>
                             <td>{{ usuario.nombre }}</td>
                             <td>{{ usuario.correo }}</td>
@@ -41,11 +51,63 @@
                                 <button class="btn btn-primary">Editar</button>
                             </td>
                             <td class="text-center">
-                                <button class="btn btn-danger">Eliminar</button>
+                                <button
+                                    class="btn btn-danger"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#modalElimina"
+                                    @click="modalEliminar(usuario)"
+                                >
+                                    Eliminar
+                                </button>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+    <!-- MODAL ELIMINAR USUARIO -->
+    <div
+        class="modal fade"
+        id="modalElimina"
+        tabindex="-1"
+        aria-labelledby="modalEliminaLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content" v-if="usuarioSeleccionado">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalEliminaLabel">
+                        Eliminar Usuario
+                    </h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    ¿Estas seguro de eliminar a
+                    {{ usuarioSeleccionado.nombre }}?
+                </div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn astra-btn-primario"
+                        data-bs-dismiss="modal"
+                    >
+                        Cancelar
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-danger"
+                        data-bs-dismiss="modal"
+                        @click="eliminarUsuario(usuarioSeleccionado.id)"
+                    >
+                        Eliminar usuario
+                    </button>
+                </div>
             </div>
         </div>
     </div>
@@ -56,6 +118,8 @@ export default {
     data() {
         return {
             usuarios: null,
+            filtro: "",
+            usuarioSeleccionado: null,
         };
     },
     methods: {
@@ -66,10 +130,44 @@ export default {
                     .select();
                 if (error) throw error;
                 this.usuarios = data;
-                console.log(this.usuarios);
             } catch (error) {
                 console.log(error);
             }
+        },
+
+        async eliminarUsuario(usuarioId) {
+            try {
+                const { data: eliminado, error } = await supabaseAdmin
+                    .from("perfil_usuario")
+                    .delete()
+                    .match({ id: usuarioId });
+                if (error) throw error;
+                if (eliminado[0].id) {
+                    const { error } = await supabaseAdmin.auth.api.deleteUser(
+                        eliminado[0].id,
+                        process.env.VUE_APP_ASTRAVITA_SUPABASE_SERVICE_KEY
+                    );
+                    if (error) throw error;
+                }
+                this.usuarios = this.usuarios.filter(
+                    (usuario) => usuario.id != usuarioId
+                );
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        modalEliminar(usuario) {
+            this.usuarioSeleccionado = usuario;
+        },
+    },
+    computed: {
+        usuariosFiltrados() {
+            return this.usuarios.filter((usuario) => {
+                return usuario.nombre
+                    .toLowerCase()
+                    .match(this.filtro.toLowerCase());
+            });
         },
     },
     async created() {
